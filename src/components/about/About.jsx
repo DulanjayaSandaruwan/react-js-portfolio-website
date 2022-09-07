01a7b3901aaca62c6573e98e08e36f6d5bf92675
DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2022-09-07 21:59:14 +0530

    end about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
 import './About.css'
+import ME from '../../assets/me-about.jpeg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
     <section id='about'>
-      About Me
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1 Year Working</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>5+ Worldwide</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>40+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            I'm Dulanjaya Sandaruwan.I'm a Fullstack Developer & Web Designer from Ayagama, Rathnapura,
+            Sri lanka who is 23 years old. I'm currently studying software engineering in Institute of
+            Software Engineering.Always looking forward for working on newest projects and career opportunities.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+
+        </div>
+      </div>
     </section>
   )
 }
