52530b0dace6471795ef2cafb266632dd5dc9d2a
DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2022-09-08 10:51:20 +0530

    fix a responsive error

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -18,7 +18,7 @@ const Portfolio = () => {
           <div className="portfolio__item-image">
             <img src={IMG1} alt="" />
           </div>
-          <h3>This is a portfolio item title</h3>
+          <h3>Farming Management System</h3>
           <div className="portfolio__item-cta">
             <a href="https://github.com" className='btn' target='_blank'>Github</a>
             <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
