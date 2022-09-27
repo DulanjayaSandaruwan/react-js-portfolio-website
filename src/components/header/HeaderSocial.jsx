6763f4ec2c338386dadac8101a61de5a19c40bce
DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2022-09-27 18:25:22 +0530

    add socials

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -6,9 +6,9 @@ import {FaTwitterSquare} from 'react-icons/fa'
 const HeaderSocial = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com" target="_blank"><BsLinkedin /></a>
-      <a href="https://gihub.com" target="_blank"><BsGithub /></a>
-      <a href="https://twitter.com" target="_blank"><FaTwitterSquare /></a>
+      <a href="linkedin.com/in/dulanjaya-sandaruwan" target="_blank"><BsLinkedin /></a>
+      <a href="https://github.com/DulanjayaSandaruwan" target="_blank"><BsGithub /></a>
+      <a href="https://twitter.com/DulanjayaSanda2?t=6J5VXgMw0M3KdOQRe7Uihg&s=08" target="_blank"><FaTwitterSquare /></a>
     </div>
   )
 }
